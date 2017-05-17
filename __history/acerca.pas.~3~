adec94ab93f7252d8d0d57038dc4eec0a4f74a97
Juan I. Serrichio <juan.serrichio@gmail.com>
2017-05-17 13:30:18 -0300

    agrega numero de version

---
+++ __history/acerca.pas.~3~
@@ -0,0 +1,33 @@
+unit acerca;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  Tacercade = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  acercade: Tacercade;
+
+implementation
+
+{$R *.dfm}
+
+procedure Tacercade.Button1Click(Sender: TObject);
+begin
+self.Close;
+end;
+
+end.
--- acerca.pas
+++ acerca.pas
@@ -11,9 +11,13 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Button1: TButton;
+    Label3: TLabel;
+    Label4: TLabel;
     procedure Button1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
+    function GetAppVersion:string;
   public
     { Public declarations }
   end;
@@ -29,5 +33,36 @@ procedure Tacercade.Button1Click(Sender: TObject);
 begin
 self.Close;
 end;
+
+procedure Tacercade.FormShow(Sender: TObject);
+begin
+label4.Caption:=self.GetAppVersion;
+end;
+
+function Tacercade.GetAppVersion:string;
+   var
+    Size, Size2: DWord;
+    Pt, Pt2: Pointer;
+   begin
+     Size := GetFileVersionInfoSize(PChar (ParamStr (0)), Size2);
+     if Size > 0 then
+     begin
+       GetMem (Pt, Size);
+       try
+          GetFileVersionInfo (PChar (ParamStr (0)), 0, Size, Pt);
+          VerQueryValue (Pt, '\', Pt2, Size2);
+          with TVSFixedFileInfo (Pt2^) do
+          begin
+            Result:= IntToStr (HiWord (dwFileVersionMS)) + '.' +
+                     IntToStr (LoWord (dwFileVersionMS)) + '.' +
+                     IntToStr (HiWord (dwFileVersionLS)) + '.' +
+                     IntToStr (LoWord (dwFileVersionLS));
+         end;
+       finally
+         FreeMem (Pt);
+       end;
+     end;
+   end;
+
 
 end.
